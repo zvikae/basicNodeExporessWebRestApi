0d939f6def65ccd6d0068e45d40c7594d1b1e9ff
Zvi Epstein <zvi@spectory.com>
2017-12-24 00:21:12 +0200

    get data from server

--- public/app.js
+++ public/app.js
@@ -1,5 +1,22 @@
 var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
-    $scope.firstName = "John";
-    $scope.lastName = "Doe";
-});
+app.controller('myCtrl', ['$scope', '$http', function($scope, $http) {
+  $scope.init = function () {
+    self.getPostData();
+  };
+
+  self.getDataFromServer = function (url) {
+    return $http({
+      method: 'GET',
+      url: url
+    });
+  };
+
+  self.getPostData = function () {
+    var url = 'http://localhost:8081/api/posts/2';
+    self.getDataFromServer(url).then(function (resolve) {
+      console.log(resolve.data);
+    }, function(reject) {
+      
+    });
+  }
+}]);
