7be479d666fa6e3955de3ff2ad6b6b0da7165c07
Zvi Epstein <zvi@spectory.com>
2017-12-24 22:00:45 +0200

    basic table

--- public/app.js
+++ public/app.js
@@ -2,6 +2,14 @@ var app = angular.module('myApp', []);
 app.controller('myCtrl', ['$scope', '$http', function($scope, $http) {
   $scope.init = function () {
     self.getPostData();
+    $scope.post_id = null;
+  };
+
+  self.resetErrorMsg = function () {
+    $scope.error = {
+      show: false,
+      msg: ''
+    }
   };
 
   self.getDataFromServer = function (url) {
@@ -11,12 +19,33 @@ app.controller('myCtrl', ['$scope', '$http', function($scope, $http) {
     });
   };
 
+  self.resolveGetComments = function (resolve) {
+    console.log(resolve.data.post);
+    if (resolve.data.post && resolve.data.post.comments) {
+      $scope.table_headers = Object.keys(resolve.data.post.comments[0]);
+      $scope.table_data = resolve.data.post.comments;
+    } else {
+      $scope.error = {
+        msg: 'No comments to show',
+        show: true
+      }
+    }
+  };
+
   self.getPostData = function () {
-    var url = 'http://localhost:8081/api/posts/2';
+    var url = 'http://localhost:8081/api/posts/' + $scope.post_id;
     self.getDataFromServer(url).then(function (resolve) {
-      console.log(resolve.data);
+      self.resolveGetComments(resolve);
     }, function(reject) {
-      
+      $scope.error = {
+        msg: reject,
+        show: true
+      }
     });
   }
+
+  $scope.getComments = function () {
+    self.resetErrorMsg();
+    self.getPostData();
+  };
 }]);
